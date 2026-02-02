ce2c7d4ed3aa1f8492c1acc87ffbeb96ffc674bc
Nadhinabinav <nadhinabinav17@gmail.com>
2026-02-02 12:30:14 +0530

    2nd update

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -134,20 +134,21 @@ USE_TZ = True
 
 DEBUG = True
 
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
+
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# STATIC_URL = '/static/'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA_URL='/media/'
